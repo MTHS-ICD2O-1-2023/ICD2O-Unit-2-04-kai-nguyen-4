337e04f4e6d4971755f0772928756081b5df177c
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-22 18:08:55 +0000

    triangle program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function findAreaClicked() {
+  const baseInput = parseInt(document.getElementById("base-entered").value)
+  const heightInput = parseInt(document.getElementById("height-entered").value)
+  document.getElementById("answer").innerHTML = "The area is: " + (baseInput * heightInput) / 2 + " cm<sup>2"
+}
